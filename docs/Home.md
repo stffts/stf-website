fd7d0f71db19c4a1ecbee8ab2fe55a7b0dc33d2c
stf <stf1835604246@163.com>
2024-04-29 20:39:56 +0800

    修改了默认生成的页面，将其改为涛哥的页面，并配置成功

---
+++ docs/Home.md
@@ -0,0 +1,52 @@
+---
+id: Home
+title: xxx's Wiki
+sidebar_label: 主页
+slug: /
+---
+
+:::info
+本站所有内容**遵循**[CC BY-NC 4.0](https://creativecommons.org/licenses/by/4.0/deed.zh)开源协议，仅限`非商业性`使用，转载请注明出处。
+:::
+
+Hello~
+
+我在这里记录一些知识，以免遗忘
+
+也希望能对你有用
+
+请随意浏览～
+
+
+<details>
+  <summary>个人简介</summary>
+  <div>
+    <div>
+        ...<br/>
+        ...<br/>
+    </div>
+  </div>
+</details>
+
+<details>
+  <summary>相关技能</summary>
+  <div>
+    <div>
+        ...<br/>
+        ...<br/>
+    </div>
+  </div>
+</details>
+
+
+<details>
+  <summary>联系方式</summary>
+  <div>
+    <div>
+        ...<br/>
+        ...<br/>
+    </div>
+  </div>
+</details>
+
+###### xx xxxx | 2022 | Built with [Docusaurus](https://www.docusaurus.cn/docs)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,135 +1,113 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
+const math = require('remark-math');
+const katex = require('rehype-katex');
 import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'STF Site',
+  tagline: '^_^',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://stf.top',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
+  themes: [
+    // ... Your other themes.
+    [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        {
+            hashed: true,
+            language: ["en", "zh"],
+            indexDocs: true,
+            indexBlog: true,
+            indexPages: true,
+        },
+    ],
+  ],
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        gtag: {
+          trackingID: 'G-XXXXXXXX',
+          anonymizeIP: false, 
         },
+/*
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarCollapsible: true,
+          routeBasePath: "/",
+          showLastUpdateTime: false,
+          showLastUpdateAuthor: false,
+          breadcrumbs: false,
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+*/
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: "/",
+          showLastUpdateTime: false,
+          showLastUpdateAuthor: false,
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
         theme: {
-          customCss: './src/css/custom.css',
-        },
+          customCss: require.resolve('./src/css/custom.css'),
+      },
       }),
     ],
+  ],
+  stylesheets: [{
+    href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+    type: 'text/css',
+    integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+    crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
+      
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'STF Site',
+        hideOnScroll: true,
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "explore",
+          src: "img/explore.svg",
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+        items: [{
+            href: "https://space.bilibili.com/35171315",
+            label: "B站主页",
+            position: "right",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/yltzdhbc",
+            label: "GITHUB",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        style: 'light',
+        links: [{
+            title: '朋友',
+            items: [{
+                href: 'https://zhangshitao.top',
+                label: '强烈推荐！：张工的博客',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://blog.csdn.net/qq_44343584',
+                label: '强烈推荐！：周工的博客',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
@@ -138,4 +116,6 @@ const config = {
     }),
 };
 
-export default config;
+//export default config;
+
+module.exports = config;
